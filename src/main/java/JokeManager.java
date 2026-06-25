a881640673a74cb075f31b8ad107cc0f44bb3304
Maria <minervalafuente@hotmail.com>
2026-06-25 13:14:03 +0200

    Documentation of the code in Main and JokeManager

--- src/main/java/JokeManager.java
+++ src/main/java/JokeManager.java
@@ -1,5 +1,17 @@
 import java.util.*;
 
+/**
+ * Creation of a jokebook through the use of an Arraylist of Strings.
+ *
+ * Features: Can display a random joke from the lot
+ * Features: Can add new jokes to the catalog
+ * Features: Can display (a preview of -future implementation) the jokes already on it
+ *
+ * @author Maria Lafuente
+ * @version 1.0
+ * @since 1.0
+ */
+
 public class JokeManager {
 
     private ArrayList<String> jokeBook;
@@ -8,9 +20,19 @@ public class JokeManager {
         this.jokeBook = new ArrayList<>();
     }
 
+    /**
+     * Adds a String to the Arraylist jokeBook
+     * @param joke
+     */
+
     public void addJoke (String joke){
         jokeBook.add(joke);
     }
+
+    /**
+     * Draws a random joke from the Arraylist
+     * @return a String, or the intended joke or an error message if it's empty
+     */
 
     public String drawJokes (){
 
@@ -24,6 +46,9 @@ public class JokeManager {
         }
     }
 
+    /**
+     * Prints all of the jokes in the console
+     */
     public void printJokes (){
 
         if (jokeBook.isEmpty()) {
